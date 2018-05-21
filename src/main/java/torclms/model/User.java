23fcd268e882710566892124a06715408dd27cc6
Michael Griffin <mdgriffin064@gmail.com>
2018-05-21 21:47:31 +0100

    Added basic user model stub

---
+++ src/main/java/torclms/model/User.java
@@ -0,0 +1,36 @@
+package torclms.model;
+
+/*
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+*/
+
+import java.io.Serializable;
+import java.util.Date;
+
+//@Entity
+//@Table(name = "users")
+//@EntityListeners(AuditingEntityListener.class)
+//@JsonIgnoreProperties(value = {"createdAt"}, allowGetters = true)
+public class User implements Serializable {
+    //@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    //@NotBlank
+    private String firstname;
+
+    //@NotBlank
+    private String surname;
+
+    //@Column(nullable = false, updatable = false)
+    //@Temporal(TemporalType.TIMESTAMP)
+    //@CreatedDate
+    private Date createdAt;
+
+
+}

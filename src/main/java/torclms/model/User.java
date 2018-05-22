4352bf90645b1a0c9612b428e271ca74e0089931
Michael Griffin <mdgriffin064@gmail.com>
2018-05-22 21:26:54 +0100

    Added getters and setters to user model

--- src/main/java/torclms/model/User.java
+++ src/main/java/torclms/model/User.java
@@ -1,36 +1,65 @@
 package torclms.model;
 
-/*
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-*/
 
 import java.io.Serializable;
 import java.util.Date;
 import javax.persistence.Entity;
 
 @Entity
-//@Table(name = "users")
-//@EntityListeners(AuditingEntityListener.class)
-//@JsonIgnoreProperties(value = {"createdAt"}, allowGetters = true)
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+//@JsonIgnoreProperties(value = {"regiseredOn"}, allowGetters = true)
 public class User implements Serializable {
-    //@Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 
-    //@NotBlank
+    @NotBlank
     private String firstname;
 
-    //@NotBlank
+    @NotBlank
     private String surname;
 
-    //@Column(nullable = false, updatable = false)
-    //@Temporal(TemporalType.TIMESTAMP)
-    //@CreatedDate
-    private Date createdAt;
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date registeredOn;
 
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getRegisteredOn() {
+        return registeredOn;
+    }
+
+    public void setRegisteredOn(Date registeredOn) {
+        this.registeredOn = registeredOn;
+    }
 }

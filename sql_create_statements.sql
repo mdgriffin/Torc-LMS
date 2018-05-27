e50b07211f0bc47d211b485deb8d9519b8d95b0a
Michael Griffin <mdgriffin064@gmail.com>
2018-05-27 21:43:24 +0100

    Added user roles table

--- sql_create_statements.sql
+++ sql_create_statements.sql
@@ -7,3 +7,12 @@ CREATE TABLE users (
   registered_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (user_id)
 );
+
+CREATE TABLE user_roles (
+  user_role_id int NOT NULL AUTO_INCREMENT,
+  user_id int NOT NULL,
+  role varchar(45) NOT NULL,
+  PRIMARY KEY (user_role_id),
+  KEY fk_user_id (user_id),
+  CONSTRAINT fk_user_roles_user FOREIGN KEY (user_id) REFERENCES users (user_id)
+);

18a5e7534d26ad6b4b48d450e7e5bc9558506a17
Michael Griffin <mdgriffin064@gmail.com>
2018-05-27 22:43:11 +0100

    Added User Role Repository

---
+++ src/main/java/torclms/repository/UserRoleRepository.java
@@ -0,0 +1,11 @@
+package torclms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import torclms.model.User;
+import torclms.model.UserRole;
+
+@Repository("UserRoleRepository")
+public interface UserRoleRepository  extends JpaRepository<UserRole, Integer> {
+}

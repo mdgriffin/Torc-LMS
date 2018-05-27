388809510d7e334640158b3d7578ea071277e1c0
Michael Griffin <mdgriffin064@gmail.com>
2018-05-27 20:42:09 +0100

    Fixed issue with Thymeleaf templates not working

--- src/main/java/torclms/controller/LoginController.java
+++ src/main/java/torclms/controller/LoginController.java
@@ -1,18 +1,15 @@
 package torclms.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 
-@RestController
-@RequestMapping("/login")
+@Controller
 public class LoginController {
 
-
-    // Get All Users
-    @GetMapping("/")
-    public String getIndex (Model model) {
+    @RequestMapping("/login")
+    public String login (Model model) {
 
         model.addAttribute("title", "Please Login");
         return "login";

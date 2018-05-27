9de251f8766efea732886c3667833fca3f0dbb81
Michael Griffin <mdgriffin064@gmail.com>
2018-05-27 21:28:38 +0100

    Added email and password to user create statement

--- sql_create_statements.sql
+++ sql_create_statements.sql
@@ -1,7 +1,9 @@
 CREATE TABLE users (
   user_id int NOT NULL auto_increment,
-  firstname VARCHAR(255) NOT NULL ,
-  surname VARCHAR(255) NOT NULL ,
+  firstname VARCHAR(255) NOT NULL,
+  surname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
   registered_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (user_id)
 );

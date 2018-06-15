2552f07b4a5f6ee3d59046b25f83de50e0c5dceb
Michael Griffin <mdgriffin064@gmail.com>
2018-06-15 11:35:42 +0100

    Changed default packaging from jar to war

--- src/main/java/torclms/Application.java
+++ src/main/java/torclms/Application.java
@@ -2,8 +2,11 @@ package torclms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/*
 @SpringBootApplication
 @EnableJpaAuditing
 public class Application {
@@ -12,4 +15,19 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+}
+*/
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 }

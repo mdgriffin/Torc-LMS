27b5130c357e5f12d6a5cd1a76cae04020953e0c
Michael Griffin <mdgriffin064@gmail.com>
2018-06-15 21:13:31 +0100

    Moved user registration page into admin area

--- src/main/java/torclms/controller/AdminController.java
+++ src/main/java/torclms/controller/AdminController.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import torclms.entity.UserRole;
 import torclms.model.User;
 import torclms.service.UserService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/admin")
@@ -29,8 +32,41 @@ public class AdminController {
         return modelAndView;
     }
 
-    @GetMapping("/create-course")
+    @GetMapping("/courses/new")
     public String createCourse () {
         return "admin/create-course";
+    }
+
+    @GetMapping("/users/new")
+    public String registration(Model model){
+        //model.addAttribute("vm", new UserRegistrationVM());
+        model.addAttribute("user", new User());
+        model.addAttribute("roles", UserRole.values());
+
+        return "registration";
+    }
+
+    @PostMapping("/users-new")
+    public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult, @RequestParam("user_role") String roleName) {
+        ModelAndView modelAndView = new ModelAndView();
+        // TODO: Selected role should be returned
+        modelAndView.addObject("roles",UserRole.values());
+        User userExists = userService.findUserByEmail(user.getEmail());
+
+        if (userExists != null) {
+            bindingResult.rejectValue("email", "error.user","There is already a user registered with the email provided");
+        }
+
+        if (bindingResult.hasErrors() || !UserRole.contains(roleName)) {
+            modelAndView.setViewName("registration");
+        } else {
+            UserRole userRole = UserRole.valueOf(roleName);
+            userService.saveUser(user, userRole);
+            modelAndView.addObject("successMessage", "User has been registered successfully");
+            modelAndView.addObject("user", new User());
+            modelAndView.setViewName("registration");
+        }
+
+        return modelAndView;
     }
 }

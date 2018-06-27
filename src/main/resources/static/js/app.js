c401e44f7e240c08415edd48acf79a731827d256
Michael Griffin <mdgriffin064@gmail.com>
2018-06-27 22:08:13 +0100

    Added click outside directive and updated vue

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,11 +1,30 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        quizTimeRemaining: 1000 * 60,// * 5
-        quizQuestions: globData.quizQuestions
+Vue.directive('click-outside', {
+    bind: function(el, binding, vNode) {
+        // Provided expression must evaluate to a function.
+        if (typeof binding.value !== 'function') {
+            const compName = vNode.context.name
+            let warn = `[Vue-click-outside:] provided expression '${binding.expression}' is not a function, but has to be`
+            if (compName) { warn += `Found in component '${compName}'` }
+
+            console.warn(warn)
+        }
+        // Define Handler and cache it on the element
+        const bubble = binding.modifiers.bubble
+        const handler = (e) => {
+            if (bubble || (!el.contains(e.target) && el !== e.target)) {
+                binding.value(e)
+            }
+        }
+        el.__vueClickOutside__ = handler
+
+        // add Event Listeners
+        document.addEventListener('click', handler)
     },
-    methods: {},
-    created: function () {
-        console.log('Vue App Created');
+
+    unbind: function(el, binding) {
+        // Remove Event Listeners
+        document.removeEventListener('click', el.__vueClickOutside__)
+        el.__vueClickOutside__ = null
+
     }
 });

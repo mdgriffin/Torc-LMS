ccbc67dc2bd9a0227cb7d48aff410a925b9b648f
Michael Griffin <mdgriffin064@gmail.com>
2018-07-02 21:46:25 +0100

    Made Trainee home page consistent with Admin and Manager home pages

--- src/main/java/torclms/controller/TraineeController.java
+++ src/main/java/torclms/controller/TraineeController.java
@@ -1,16 +1,31 @@
 package torclms.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import torclms.model.User;
+import torclms.service.UserService;
 
 @Controller
 @RequestMapping("/learn")
 public class TraineeController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping(value={"/", "/home"})
-    public String home () {
-        return "learn/home";
+    public ModelAndView home () {
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        modelAndView.addObject("userName", "Welcome " + user.getFirstname() + " " + user.getSurname() + " (" + user.getEmail() + ")");
+        modelAndView.addObject("traineeMessage","Content Available Only for Users with Trainee Role");
+        modelAndView.setViewName("learn/home");
+        return modelAndView;
     }
 
 }

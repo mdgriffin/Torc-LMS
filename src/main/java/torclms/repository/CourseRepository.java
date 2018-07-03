23004da4769a8ac1d6c8776688e3027ea8cc3d67
Michael Griffin <mdgriffin064@gmail.com>
2018-07-03 13:19:41 +0100

    Getting stages assigned to user

--- src/main/java/torclms/repository/CourseRepository.java
+++ src/main/java/torclms/repository/CourseRepository.java
@@ -1,8 +1,18 @@
 package torclms.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import torclms.model.Course;
 
+import java.util.Date;
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Integer> {
+
     Course findByCourseId (int courseId);
+
+    @Query("SELECT c FROM Course c INNER JOIN c.assignedUsers a INNER JOIN a.assignedUser u WHERE u.userId = :userId AND a.assignedOn <= :startDate AND a.deadline <= :endDate")
+    List<Course> findAssignedCourses (@Param("userId") Long userId, @Param("startDate") Date startDate, @Param("endDate") Date endDate);
+
 }

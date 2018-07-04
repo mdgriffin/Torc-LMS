ed546d493c183ce5d3091430fab00ad17d23580b
Michael Griffin <mdgriffin064@gmail.com>
2018-07-04 22:22:34 +0100

    Moved assigned course view from html to js

--- src/main/java/torclms/controller/TraineeController.java
+++ src/main/java/torclms/controller/TraineeController.java
@@ -20,10 +20,6 @@ public class TraineeController {
     @GetMapping(value={"/", "/home"})
     public ModelAndView home () {
         ModelAndView modelAndView = new ModelAndView();
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        User user = userService.findUserByEmail(auth.getName());
-        modelAndView.addObject("userName", "Welcome " + user.getFirstname() + " " + user.getSurname() + " (" + user.getEmail() + ")");
-        modelAndView.addObject("traineeMessage","Content Available Only for Users with Trainee Role");
         modelAndView.setViewName("learn/home");
         return modelAndView;
     }

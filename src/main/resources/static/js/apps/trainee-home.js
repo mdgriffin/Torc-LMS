5569f6ccd6998947fc64981e25cadb822fdbd7aa
Michael Griffin <mdgriffin064@gmail.com>
2018-08-11 21:56:38 +0100

    Calculating the current stage index in order to resume stage
    Displaying the percentage of the course completed
    Fixed info message flash

--- src/main/resources/static/js/apps/trainee-home.js
+++ src/main/resources/static/js/apps/trainee-home.js
@@ -8,13 +8,14 @@ var TraineeHomeApp = (function () {
                 <loading-status v-if="assignmentsLoading"></loading-status>
         
                 <div class="userAssignments">
-                    <div v-for="assignment in userAssignments" class="userAssignments-single" v-if="assignment.status === 'INCOMPLETE'">
+                    <div v-for="(assignment, assignmentIndex) in userAssignments" class="userAssignments-single" v-if="assignment.status === 'INCOMPLETE'">
                         <img v-if="assignment.assignedCourse.imageName !== null" :src="cdnUrl + '/images/' + assignment.assignedCourse.imageName" alt="">
                         <img v-else :src="contextRoot + '/images/placeholder.jpeg'" alt="">
                         <div class="userAssignments-single-body">
                             <h3>{{ assignment.assignedCourse.title }}</h3>
                             <h4>Deadline: {{ assignment.deadline | dateFormat }}</h4>
-                            <a :href="'assignment/' + assignment.userAssignmentId">Start Course</a>
+                            <p>{{ getPercentageCompleted(assignmentIndex)}} completed</p>
+                            <a :href="'assignment/' + assignment.userAssignmentId">{{getCurrentStageIndex(assignmentIndex) === 0 ? 'Start' : 'Resume'}} Course</a>
                         </div>
                     </div>
                 </div>
@@ -34,6 +35,30 @@ var TraineeHomeApp = (function () {
                 cdnUrl: Config.cdnUrl
             }
         },
+        methods: {
+            getCurrentStageIndex: function (assignmentIndex) {
+                let assignment = this.userAssignments[assignmentIndex];
+
+                if (assignment.currentStageId) {
+                    let currentStageIndex = -1;
+                    assignment.assignedCourse.stages.forEach((stage, stageIndex) => {
+                        if (stage.stageId === assignment.currentStageId) {
+                            currentStageIndex = stageIndex;
+                        }
+                    });
+                    if (currentStageIndex !== -1) {
+                        return currentStageIndex;
+                    }
+                }
+                return 0;
+            },
+            getPercentageCompleted (assignmentIndex) {
+                let currentStageIndex = this.getCurrentStageIndex(assignmentIndex);
+                let numStages  = this.userAssignments[assignmentIndex].assignedCourse.stages.length;
+
+                return (numStages > 0 ? (currentStageIndex / numStages) * 100 : 0) + '%';
+            }
+        },
         components: {
             'loading-status': LoadingStatus
         },
@@ -48,7 +73,6 @@ var TraineeHomeApp = (function () {
             fetch(Config.assignedCoursesApiUrl, {
                 credentials: "include"
             }).then(response => {
-                self.assignmentsLoading = false;
                 if (response.ok) {
                     return response.json();
                 } else {
@@ -58,6 +82,7 @@ var TraineeHomeApp = (function () {
                 if (json) {
                     self.userAssignments = json;
                 }
+                self.assignmentsLoading = false;
             }).catch(error => {
                 console.error('Error', error);
                 self.assignmentsLoading = false;

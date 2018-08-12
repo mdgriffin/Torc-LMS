be28fdb2d10b69e7f4a2caaa205991e9fed9cc50
Michael Griffin <mdgriffin064@gmail.com>
2018-08-12 20:53:18 +0100

    Displaying locked and completed home on trainee home page

--- src/main/resources/static/js/apps/trainee-home.js
+++ src/main/resources/static/js/apps/trainee-home.js
@@ -3,24 +3,62 @@ var TraineeHomeApp = (function () {
     var template = `
         <article class="card">
             <div class="card-body">
-                <h3>Assigned Course</h3>
         
                 <loading-status v-if="assignmentsLoading"></loading-status>
         
-                <div class="userAssignments">
-                    <div v-for="(assignment, assignmentIndex) in userAssignments" class="userAssignments-single" v-if="assignment.status === 'INCOMPLETE'">
-                        <img v-if="assignment.assignedCourse.imageName !== null" :src="cdnUrl + '/images/' + assignment.assignedCourse.imageName" alt="">
-                        <img v-else :src="contextRoot + '/images/placeholder.jpeg'" alt="">
-                        <div class="userAssignments-single-body">
-                            <h3>{{ assignment.assignedCourse.title }}</h3>
-                            <h4>Deadline: {{ assignment.deadline | dateFormat }}</h4>
-                            <p>{{ getPercentageCompleted(assignmentIndex)}} completed</p>
-                            <a :href="'assignment/' + assignment.userAssignmentId">{{getCurrentStageIndex(assignmentIndex) === 0 ? 'Start' : 'Resume'}} Course</a>
+                <div class="userAssignments" v-if="!assignmentsLoading">
+                
+                    <div class="userAssignments-section">
+                
+                        <h3>Assigned Course</h3>
+                        <div v-for="(assignment, assignmentIndex) in incompleteAssignments" class="userAssignments-single">
+                            <img v-if="assignment.assignedCourse.imageName !== null" :src="cdnUrl + '/images/' + assignment.assignedCourse.imageName" alt="">
+                            <img v-else :src="contextRoot + '/images/placeholder.jpeg'" alt="">
+                            <div class="userAssignments-single-body">
+                                <h4>{{ assignment.assignedCourse.title }}</h4>
+                                <h4>Deadline: {{ assignment.deadline | dateFormat }}</h4>
+                                <p>{{ getPercentageCompleted(assignmentIndex)}} completed</p>
+                                <a :href="'assignment/' + assignment.userAssignmentId">{{getCurrentStageIndex(assignmentIndex) === 0 ? 'Start' : 'Resume'}} Course</a>
+                            </div>
                         </div>
+
+                        <div class="alert alert-info" v-if="incompleteAssignments.length === 0">You are currently not assigned any course</div>
+
                     </div>
+                    
+                    <div class="userAssignments-section">
+                    
+                        <h3>Completed Assignments</h3>
+                    
+                        <div v-for="(assignment, assignmentIndex) in completedAssignments" class="userAssignments-single">
+                            <img v-if="assignment.assignedCourse.imageName !== null" :src="cdnUrl + '/images/' + assignment.assignedCourse.imageName" alt="">
+                            <img v-else :src="contextRoot + '/images/placeholder.jpeg'" alt="">
+                            <div class="userAssignments-single-body">
+                                <h4>{{ assignment.assignedCourse.title }}</h4>
+                            </div>
+                        </div>
+                        
+                        <div class="alert alert-info" v-if="completedAssignments.length === 0">You have not completed any courses</div>
+                    
+                    </div>
+                    
+                    <div class="userAssignments-section" v-if="lockedAssignments.length > 0">
+                    
+                         <h3>Locked Assignments</h3>
+                        
+                        <div v-for="(assignment, assignmentIndex) in lockedAssignments" class="userAssignments-single">
+                            <img v-if="assignment.assignedCourse.imageName !== null" :src="cdnUrl + '/images/' + assignment.assignedCourse.imageName" alt="">
+                            <img v-else :src="contextRoot + '/images/placeholder.jpeg'" alt="">
+                            <div class="userAssignments-single-body">
+                                <h3>{{ assignment.assignedCourse.title }}</h3>
+                            </div>
+                        </div>
+
+                    </div>
+                
                 </div>
                 
-                <div class="alert alert-info" v-if="!assignmentsLoading && userAssignments.length === 0">There are currently no assigned courses</div>
+                
             </div>
         </article>
     `
@@ -33,6 +71,17 @@ var TraineeHomeApp = (function () {
                 userAssignments: [],
                 contextRoot: Config.contextRoot,
                 cdnUrl: Config.cdnUrl
+            }
+        },
+        computed: {
+            incompleteAssignments: function () {
+                return this.userAssignments.filter(assignment => assignment.status === 'INCOMPLETE');
+            },
+            completedAssignments: function () {
+                return this.userAssignments.filter(assignment => assignment.status === 'COMPLETED');
+            },
+            lockedAssignments: function () {
+                return this.userAssignments.filter(assignment => assignment.status === 'LOCKED');
             }
         },
         methods: {
